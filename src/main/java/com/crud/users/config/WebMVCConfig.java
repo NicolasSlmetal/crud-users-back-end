00e88bb5e5b27914905d86a4541ab82a553d3223
Nicolas Lima <nlimadasilvabrito@gmail.com>
2025-08-13 14:16:49 -0300

    feat: add WebMVCConfig for CORS configuration

---
+++ src/main/java/com/crud/users/config/WebMVCConfig.java
@@ -0,0 +1,25 @@
+package com.crud.users.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMVCConfig {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
