b0bbaf7f67415d24344b3a4f8720e46eef8ea2f5
Nicolas Lima <nlimadasilvabrito@gmail.com>
2025-08-16 14:58:57 -0300

    feat: include addresses fields to ViaCepResponseDTO

--- src/main/java/com/crud/users/dtos/external/ViaCepResponseDTO.java
+++ src/main/java/com/crud/users/dtos/external/ViaCepResponseDTO.java
@@ -1,6 +1,19 @@
 package com.crud.users.dtos.external;
 
 public record ViaCepResponseDTO(
-        String erro
+        String erro,
+        String cep,
+        String logradouro,
+        String bairro,
+        String localidade,
+        String estado
 ) {
+
+    public ViaCepResponseDTO(String erro) {
+        this(erro, null, null, null, null, null);
+    }
+
+    public ViaCepResponseDTO(String cep, String logradouro, String bairro, String localidade, String estado) {
+        this(null, cep, logradouro, bairro, localidade, estado);
+    }
 }
