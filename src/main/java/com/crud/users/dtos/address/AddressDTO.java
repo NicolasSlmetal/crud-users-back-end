89558df3302a9fd85bc4b64ed7f8ca2ec61f3742
Nicolas Lima <nlimadasilvabrito@gmail.com>
2025-08-16 14:58:03 -0300

    refactor: include JSONInclude annotation to avoid null fields to be exposed

--- src/main/java/com/crud/users/dtos/address/AddressDTO.java
+++ src/main/java/com/crud/users/dtos/address/AddressDTO.java
@@ -1,24 +1,43 @@
 package com.crud.users.dtos.address;
 
+import com.crud.users.dtos.external.ViaCepResponseDTO;
 import com.crud.users.entities.Address;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record AddressDTO(
         Long id,
         String cep,
         String street,
         String number,
-        String state,
+        String neighborhood,
         String city,
-        String neighborhood
+        String state
 ) {
+
+    public AddressDTO {
+        if (cep != null) {
+            cep = cep.replaceAll("[^0-9]", "");
+        }
+    }
 
     public AddressDTO(Address address) {
         this(address.getId(),
                 address.getCep(),
                 address.getStreet(),
                 address.getNumber(),
-                address.getState(),
+                address.getNeighborhood(),
                 address.getCity(),
-                address.getNeighborhood());
+                address.getState());
+    }
+
+    public AddressDTO(ViaCepResponseDTO viaCepResponseDTO) {
+        this(null,
+                viaCepResponseDTO.cep(),
+                viaCepResponseDTO.logradouro(),
+                null,
+                viaCepResponseDTO.bairro(),
+                viaCepResponseDTO.localidade(),
+                viaCepResponseDTO.estado());
     }
 }
